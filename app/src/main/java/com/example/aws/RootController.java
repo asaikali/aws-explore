d1de21bc71cf751f6419cdb72611cc7c53422405
Adib Saikali <asaikali@gmail.com>
2026-01-07 23:00:54 -0500

    Retrieve Account Aliases

--- app/src/main/java/com/example/aws/RootController.java
+++ app/src/main/java/com/example/aws/RootController.java
@@ -4,6 +4,8 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.services.iam.IamClient;
+import software.amazon.awssdk.services.iam.model.ListAccountAliasesResponse;
 import software.amazon.awssdk.services.sts.StsClient;
 import software.amazon.awssdk.services.sts.model.GetCallerIdentityResponse;
 
@@ -11,14 +13,18 @@ import software.amazon.awssdk.services.sts.model.GetCallerIdentityResponse;
 public class RootController {
 
   @GetMapping("/")
-  Map<String, String> whoAmI() {
+  Map<String, Object> whoAmI() {
     ProfileCredentialsProvider  creds = ProfileCredentialsProvider.create("default");
     StsClient sts = StsClient.builder().credentialsProvider(creds).build();
     GetCallerIdentityResponse callerIdentity = sts.getCallerIdentity();
 
+    IamClient iam = IamClient.builder().credentialsProvider(creds).build();
+    ListAccountAliasesResponse aliases = iam.listAccountAliases();
+
     return Map.of("account", callerIdentity.account(),
         "ARN", callerIdentity.arn(),
-        "userId", callerIdentity.userId());
+        "userId", callerIdentity.userId(),
+        "accountAliases",aliases.accountAliases());
 
   }
 }
